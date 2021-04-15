183622e1f40a87883e2e38019b76803d0d382310
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2021-04-15 12:11:29 -0400

    refactor services to utils

--- screens/Home/Tabs/Main.js
+++ screens/Home/Tabs/Main.js
@@ -2,45 +2,9 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Box from '../../../components/Box';
 
-const Main = () => {
-  const categories = [
-    {
-      id: 'JBUTgEcwTAbCMxz75C2f',
-      text: 'Grocery Shopping',
-      image: require('../images/undraw_grocery.png'),
-    },
-    {
-      id: 't639mNXGDrd8K6HdTH12',
-      text: 'Plumbing',
-      image: require('../images/undraw_plumbing.png'),
-    },
-    {
-      id: '0zIBXQa6yCmTgC5aSBgx',
-      text: 'Home Cleaning',
-      image: require('../images/undraw_cleaning.png'),
-    },
-    {
-      id: 'LxBSo5WOCuOVtesGzpQx',
-      text: 'Painting',
-      image: require('../images/undraw_painting.png'),
-    },
-    {
-      id: '531MKimcJpWPSJ32H05k',
-      text: 'Lawn Care',
-      image: require('../images/undraw_lawn.png'),
-    },
-    {
-      id: 'LugR3LLFyex839CqmKpR',
-      text: 'Electrician',
-      image: require('../images/undraw_electrician.png'),
-    },
-    {
-      id: 'aW6210CdOKqRu9g4sdiK',
-      text: 'Tech Help',
-      image: require('../images/undraw_tech.png'),
-    },
-  ];
+import categories from '../../../utils/services';
 
+const Main = () => {
   return (
     <View style={styles.container}>
       {categories.map(({ id, text, image }) => (
