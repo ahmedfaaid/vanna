12963599a4e8d4ff3508cf9f3938031d56c78434
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2021-04-15 13:01:32 -0400

    close button and styles

--- screens/Modal/index.js
+++ screens/Modal/index.js
@@ -1,16 +1,45 @@
 import React from 'react';
-import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native';
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 
 const Modal = ({ navigation }) => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <TouchableOpacity onPress={() => navigation.pop()}>
-        <FontAwesome5 name="times" color="#D90429" solid />
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity
+        style={styles.closeBtnContainer}
+        onPress={() => navigation.pop()}>
+        <MaterialIcon
+          name="close"
+          color="#D90429"
+          solid
+          style={styles.closeBtn}
+        />
       </TouchableOpacity>
       <Text>Modal</Text>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 5,
+  },
+  closeBtnContainer: {
+    alignSelf: 'flex-end',
+    marginRight: 15,
+    marginTop: 10,
+  },
+  closeBtn: {
+    fontSize: 25,
+    fontWeight: '300',
+  },
+});
 
 export default Modal;
