a7ad5dfad64dea0fc9723b76cd0f7777e4685df2
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2021-04-15 13:40:53 -0400

    add generic packages to services

--- utils/services.js
+++ utils/services.js
@@ -3,35 +3,175 @@ module.exports = [
     id: 'JBUTgEcwTAbCMxz75C2f',
     text: 'Grocery Shopping',
     image: require('../screens/Home/images/undraw_grocery.png'),
+    packages: [
+      {
+        id: 1,
+        name: 'Package 1',
+        description: 'Lorem ipsum...',
+        price: 80.0,
+      },
+      {
+        id: 2,
+        name: 'Package 2',
+        description: 'Lorem ipsum',
+        price: 110.0,
+      },
+      {
+        id: 3,
+        name: 'Package 3',
+        description: 'Lorem ipsum',
+        price: 150.0,
+      },
+    ],
   },
   {
     id: 't639mNXGDrd8K6HdTH12',
     text: 'Plumbing',
     image: require('../screens/Home/images/undraw_plumbing.png'),
+    packages: [
+      {
+        id: 1,
+        name: 'Package 1',
+        description: 'Lorem ipsum...',
+        price: 80.0,
+      },
+      {
+        id: 2,
+        name: 'Package 2',
+        description: 'Lorem ipsum',
+        price: 110.0,
+      },
+      {
+        id: 3,
+        name: 'Package 3',
+        description: 'Lorem ipsum',
+        price: 150.0,
+      },
+    ],
   },
   {
     id: '0zIBXQa6yCmTgC5aSBgx',
     text: 'Home Cleaning',
     image: require('../screens/Home/images/undraw_cleaning.png'),
+    packages: [
+      {
+        id: 1,
+        name: 'Package 1',
+        description: 'Lorem ipsum...',
+        price: 80.0,
+      },
+      {
+        id: 2,
+        name: 'Package 2',
+        description: 'Lorem ipsum',
+        price: 110.0,
+      },
+      {
+        id: 3,
+        name: 'Package 3',
+        description: 'Lorem ipsum',
+        price: 150.0,
+      },
+    ],
   },
   {
     id: 'LxBSo5WOCuOVtesGzpQx',
     text: 'Painting',
     image: require('../screens/Home/images/undraw_painting.png'),
+    packages: [
+      {
+        id: 1,
+        name: 'Package 1',
+        description: 'Lorem ipsum...',
+        price: 80.0,
+      },
+      {
+        id: 2,
+        name: 'Package 2',
+        description: 'Lorem ipsum',
+        price: 110.0,
+      },
+      {
+        id: 3,
+        name: 'Package 3',
+        description: 'Lorem ipsum',
+        price: 150.0,
+      },
+    ],
   },
   {
     id: '531MKimcJpWPSJ32H05k',
     text: 'Lawn Care',
     image: require('../screens/Home/images/undraw_lawn.png'),
+    packages: [
+      {
+        id: 1,
+        name: 'Package 1',
+        description: 'Lorem ipsum...',
+        price: 80.0,
+      },
+      {
+        id: 2,
+        name: 'Package 2',
+        description: 'Lorem ipsum',
+        price: 110.0,
+      },
+      {
+        id: 3,
+        name: 'Package 3',
+        description: 'Lorem ipsum',
+        price: 150.0,
+      },
+    ],
   },
   {
     id: 'LugR3LLFyex839CqmKpR',
     text: 'Electrician',
     image: require('../screens/Home/images/undraw_electrician.png'),
+    packages: [
+      {
+        id: 1,
+        name: 'Package 1',
+        description: 'Lorem ipsum...',
+        price: 80.0,
+      },
+      {
+        id: 2,
+        name: 'Package 2',
+        description: 'Lorem ipsum',
+        price: 110.0,
+      },
+      {
+        id: 3,
+        name: 'Package 3',
+        description: 'Lorem ipsum',
+        price: 150.0,
+      },
+    ],
   },
   {
     id: 'aW6210CdOKqRu9g4sdiK',
     text: 'Tech Help',
     image: require('../screens/Home/images/undraw_tech.png'),
+    packages: [
+      {
+        id: 1,
+        name: 'Package 1',
+        description: 'Lorem ipsum...',
+        price: 80.0,
+      },
+      {
+        id: 2,
+        name: 'Package 2',
+        description: 'Lorem ipsum',
+        price: 110.0,
+      },
+      {
+        id: 3,
+        name: 'Package 3',
+        description: 'Lorem ipsum',
+        price: 150.0,
+      },
+    ],
   },
 ];
