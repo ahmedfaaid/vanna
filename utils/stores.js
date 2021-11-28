8e5d2778c6d08027968e40927802dcf3451375ab
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2021-11-28 10:30:27 -0500

    Add stores list

---
+++ utils/stores.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Ghana Market',
+    image: require('../screens/Home/images/stores/veggies.png'),
+  },
+  {
+    id: 2,
+    name: 'Max Mart',
+    image: require('../screens/Home/images/stores/carrot_bw.png'),
+  },
+  {
+    id: 3,
+    name: 'Shoprite',
+    image: require('../screens/Home/images/stores/onion_bw.png'),
+  },
+  {
+    id: 4,
+    name: 'Koala',
+    image: require('../screens/Home/images/stores/vegetables_bw.png'),
+  },
+];
