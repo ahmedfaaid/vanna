a1fc2dbe74616629479d87a0aac128e0e67e7340
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2021-12-08 15:10:48 -0500

    remove all content

--- screens/Splash/index.js
+++ screens/Splash/index.js
@@ -8,49 +8,14 @@ import {
   StyleSheet,
   TouchableOpacity,
 } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Splash = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle="light-content" backgroundColor="#D90429" />
-      <View style={styles.top}>
-        <View style={styles.imageContainer}>
-          <View style={styles.inner}>
-            <Image
-              source={require('./images/undraw_visionary_technology.png')}
-              style={styles.image}
-            />
-          </View>
-        </View>
-      </View>
-      <View style={styles.bottom}>
-        <Text style={styles.getStarted}>Get Started</Text>
-        <View style={styles.btnsContainer}>
-          <View style={styles.btnWrapper}>
-            <TouchableOpacity
-              style={[styles.btn, { backgroundColor: '#D90429' }]}
-              onPress={() => navigation.navigate('SignIn')}>
-              <Text style={styles.btnText}>Sign In</Text>
-            </TouchableOpacity>
-          </View>
-          <View style={styles.btnWrapper}>
-            <TouchableOpacity
-              style={[
-                styles.btn,
-                {
-                  backgroundColor: '#FFF',
-                  borderColor: '#EF233C',
-                },
-              ]}
-              onPress={() => navigation.navigate('SignUp')}>
-              <Text style={[styles.btnText, { color: '#2B2D42' }]}>
-                Sign Up
-              </Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </View>
-    </View>
+    <SafeAreaView style={styles.container} edges={['top']}>
+      <StatusBar barStyle="light-content" />
+      <Text>Start Here!</Text>
+    </SafeAreaView>
   );
 };
 
@@ -58,62 +23,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#D90429',
-  },
-  imageContainer: {
-    flex: 1,
-    width: '60%',
-  },
-  inner: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: 'relative',
-  },
-  image: {
-    width: '100%',
-    resizeMode: 'contain',
-  },
-  top: {
-    flex: 2,
-    paddingTop: 60,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  bottom: {
-    flex: 2,
-    backgroundColor: '#FFF',
-    borderTopLeftRadius: 30,
-    borderTopRightRadius: 30,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  getStarted: {
-    color: '#2B2D42',
-    fontSize: 20,
-    textTransform: 'uppercase',
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  btnsContainer: {
-    width: '90%',
-  },
-  btnWrapper: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100%',
-  },
-  btn: {
-    marginVertical: 10,
-    paddingVertical: 10,
-    width: '80%',
-    borderColor: 'transparent',
-    borderWidth: 1,
-    borderRadius: 10,
-  },
-  btnText: {
-    textAlign: 'center',
-    fontSize: 16,
-    color: '#fff',
   },
 });
 
