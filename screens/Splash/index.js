73e5d2fc829adf9b5f9f0a6c3c1846ae660c68ba
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2021-12-08 23:07:34 -0500

    completed splash screen redesign

--- screens/Splash/index.js
+++ screens/Splash/index.js
@@ -9,20 +9,72 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import LinearGradient from 'react-native-linear-gradient';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import colors from '../../utils/colors';
 
 const Splash = ({ navigation }) => {
   return (
-    <SafeAreaView style={styles.container} edges={['top']}>
+    <LinearGradient
+      colors={[colors.yellow, colors.yellow, colors.red]}
+      style={styles.container}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 0.8, y: 1 }}>
       <StatusBar barStyle="light-content" />
-      <Text>Start Here!</Text>
-    </SafeAreaView>
+      <SafeAreaView edges={['top']} style={styles.contentContainer}>
+        <View style={{ marginBottom: 100 }}>
+          <Image
+            source={require('./images/goEasy_white.png')}
+            style={styles.logo}
+          />
+          <Text style={styles.text}>A helping hand for your home</Text>
+        </View>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => navigation.navigate('SignUp')}>
+          <Text style={styles.btnText}>Get Started </Text>
+          <Icon name="arrow-right" size={18} style={{ marginLeft: 10 }} />
+        </TouchableOpacity>
+      </SafeAreaView>
+    </LinearGradient>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#D90429',
+  },
+  contentContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 300,
+    resizeMode: 'contain',
+  },
+  text: {
+    fontSize: 23,
+    color: colors.white,
+    marginTop: -80,
+  },
+  btn: {
+    paddingVertical: 10,
+    width: '80%',
+    borderRadius: 10,
+    backgroundColor: colors.white,
+    position: 'absolute',
+    bottom: 70,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btnText: {
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: '600',
+    color: colors.black,
   },
 });
 
